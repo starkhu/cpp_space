03780699457394058d18d3a3ecf70af22f9a8877
starkhu <2360347255@qq.com>
2020-03-06 16:52:08 +0800

    feat(class): introduce =default =delete

---
+++ class/orange.h
@@ -0,0 +1,19 @@
+/*******************************************************************
+*=default 显示声明这是一个默认构造函数，如果用户自己写的构造函数比默认构造函数更高效，
+*         则可以声明自己的函数为编译器默认执行的构造函数
+*=delete  显示禁止定义某类成员函数
+*******************************************************************/
+
+
+#include<string>
+
+class Orange {
+ public:
+  Orange(); // =default; 
+  Orange(Orange&); //=delete;
+  void setName(std::string name);
+
+  private:
+    std::string name_;
+    float age_;
+};
