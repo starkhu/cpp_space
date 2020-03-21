849291f19382a3c5dcc6e7a79127141f0f2ea380
starkhu <2360347255@qq.com>
2020-03-21 17:26:13 +0800

    feat(math): add square sqrt abs fabs

---
+++ operator/math_test.cc
@@ -0,0 +1,27 @@
+/******************************************************************
+ *mathExponentialOperation指数运算：
+ * pow(): a^n
+ * sqrt(): a^0.5
+ * abs(): 整数的绝对值
+ * fabs(): 浮点数的绝对值
+ ******************************************************************/
+
+
+#include <iostream>
+#include <math.h>
+
+void mathExponentialOperation() {
+  float a = 10.5;
+  float square_a = pow(a, 2);
+  float sqrt_a = sqrt(a);
+  float abs_a = abs(-a);
+  float fabs_a = fabs(-a); 
+  std::cout << "a = " << a << ", square of a = " << square_a
+            << ", sqrt of a = " << sqrt_a << ", abs of -a = " << abs_a
+	    << ", fabs of -a = " << fabs_a << std::endl;
+}
+
+int main() {
+  mathExponentialOperation();
+  return 0;
+}
