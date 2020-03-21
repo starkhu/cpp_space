eda36d519690b8d919b61169e5a96c25c3cf2473
starkhu <2360347255@qq.com>
2020-03-21 17:25:11 +0800

    fix(format): improve format of notation

--- thread/thread_function.cc
+++ thread/thread_function.cc
@@ -1,13 +1,13 @@
 /******************************************************************
-*std::thread的常见类成员函数：
-*get_id(): 获取线程id
-*joinable(): 判断线程是否可被join
-*join()
-*detach()
-*swap
-*native_handle
-*hardware_concurrency: 检测硬件并发特性
-******************************************************************/
+ *std::thread的常见类成员函数：
+ *get_id(): 获取线程id
+ *joinable(): 判断线程是否可被join
+ *join()
+ *detach()
+ *swap
+ *native_handle
+ *hardware_concurrency: 检测硬件并发特性
+ ******************************************************************/
 
 
 #include<thread>
