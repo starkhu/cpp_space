6e791bada6cf8415aa3a585bc7a747f4f603e7c3
starkhu <2360347255@qq.com>
2020-05-24 18:26:12 +0800

    feat(thread, lock): add thread and  lock function

--- thread/thread_function.cc
+++ thread/thread_function.cc
@@ -1,18 +1,4 @@
-/******************************************************************
- *std::thread的常见类成员函数：
- *get_id(): 获取线程id
- *joinable(): 判断线程是否可被join
- *join()
- *detach()
- *swap
- *native_handle
- *hardware_concurrency: 检测硬件并发特性
- ******************************************************************/
-
-
 #include<thread>
-
-
 std::thread::id getThreadId() {
   std::thread::is tid =  std::this_thread::get_id();
   return tid;
