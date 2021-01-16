021aca8fc9e5ec1f8ffc1722f9046ece596aef97
starkhu <2360347255@qq.com>
2021-01-16 16:24:19 +0800

    feat(multiprocess): add multi process demo

---
+++ multi_processes/multi_processes.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unistd.h>
+
+int main() {
+  int i = 10;
+  std::cout << "flag1, pid is: " << getpid() << std::endl;
+  if (fork() > 0) {
+    ++i;
+    std::cout << "this is father process, pid is " << getpid() << std::endl;
+  } else {
+    if (fork() > 0) {
+    sleep(1);
+    ++i;
+    std::cout << "this is child process, pid is " << getpid() << std::endl;
+    } else {
+      sleep(1);
+      std::cout << "this is childchild process, pid is " << getpid() << std::endl; 
+    }
+  }
+  std::cout << "flag2, pid is: " << getpid() << std::endl;
+  std::cout << "i is: " << i << std::endl;
+
+}
