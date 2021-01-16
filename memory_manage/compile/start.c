afae42856bbb7552c52a3e1b83554019331cb184
starkhu <2360347255@qq.com>
2021-01-16 16:28:10 +0800

    feat(compile): add a simple compile demo

---
+++ memory_manage/compile/start.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int gdata1 = 10;
+int gdata2 = 0;
+int gdata3;
+
+static int sgdata1 = 10;
+static int sgdata2 = 0;
+static int sgdata3;
+
+int main() {
+  int a = 10;
+  int b = 0;
+  int c;
+
+  static int sdata1 = 10;
+  static int sdata2 = 0;
+  static int sdata3;
+
+  return 0;
+}
