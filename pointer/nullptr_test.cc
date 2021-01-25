74905a5b6b7fbef446b8cb7ee06843d22d710249
starkhu <2360347255@qq.com>
2021-01-25 19:28:45 +0800

    feat(pointer): add nullptr demo

---
+++ pointer/nullptr_test.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stdio.h>
+#include<typeinfo>
+
+#ifdef __cplusplus
+#define FLAG 1
+#else
+#define FLAG 0
+#endif
+
+
+namespace TestCase1 {
+
+void f1(void* n) {
+  std::cout << "void* n" << std::endl;
+}
+void f2(int n) {
+  std::cout << "f2, *n = " << n << std::endl;
+}
+
+}
+
+namespace TestCase2 {
+
+void test_case() {
+  printf("NULL: %li\n", NULL);
+  printf("sizeof(NULL): %lu\n", sizeof(NULL));
+  printf("typeid(__null).name(): %s\n", typeid(__null).name());
+  printf("typeid(0).name(): %s\n", typeid(0).name());
+}
+
+}
+
+
+int main() {
+  TestCase1::f1(NULL);
+  TestCase1::f2(NULL);
+  TestCase2::test_case();
+  return 0;
+}
